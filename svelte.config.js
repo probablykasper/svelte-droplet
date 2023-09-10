2ce608e1570f12bdc360b6a0d574b5fec8d27964
Kasper <kasperkh.kh@gmail.com>
2023-09-10 03:21:31 +0200

    Update deps

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,10 @@ import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
   },
 }
-
-export default config
